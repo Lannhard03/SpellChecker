43d281138953858764e6991dc4a029f9aca5013a
Gustav Lannhard <gustav.lannhard@gmail.com>
2025-09-28 14:22:05 +0200

    Optimzed program by changing BKtree, and dist fn:s to use [u8] instead of &str

--- src/bktree.rs
+++ src/bktree.rs
@@ -9,19 +9,19 @@ use crate::data::WordDict;
 
 pub struct BKTreeWords<'a>
 {
-    bk_tree: Arena<(&'a String,i8)>,
+    bk_tree: Arena<(&'a [u8], i8)>,
     root_id: NodeId,
-    pub dist_fn: fn(&str, &str) -> i8, 
+    pub dist_fn: fn(&[u8], &[u8]) -> i8, 
     pub dist_max: i8
 }
 
 impl<'a> BKTreeWords<'a>
 {
-    pub fn build(word_list: &'a WordDict, dist_fn: fn(&str, &str) -> i8) -> Self 
+    pub fn build(word_list: &'a WordDict, dist_fn: fn(&[u8], &[u8]) -> i8) -> Self 
     {
-        let data = word_list.get_data();
+        let data : Vec<&[u8]> = word_list.get_data().iter().map(|s| s.as_bytes()).collect();
         let mut bk_tree = Arena::with_capacity(data.len());
-        let root = bk_tree.new_node((&data[0], 0));
+        let root = bk_tree.new_node((data[0], 0));
         let mut dist_max = 0;
         let mut current_node;
         //Add each word to BK-tree structure (skip first node)
@@ -38,12 +38,15 @@ impl<'a> BKTreeWords<'a>
                 //If child with same distance is found, go one level deeper in search
                 for child in children {
                     let child_dist = bk_tree[child].get().1; //Second field is distance to parent
-                    if child_dist == dist {current_node = child; continue 'traversal;}
+                    if child_dist == dist {
+                        current_node = child;
+                        continue 'traversal;
+                    }
                 }
 
                 //If no child has the same distance, add a new child with that distance
                 dist_max = cmp::max(dist_max, dist); 
-                current_node.append_value((word, dist), &mut bk_tree);
+                current_node.append_value((*word, dist), &mut bk_tree);
                 break;
             } 
         }
@@ -65,13 +68,13 @@ impl<'a> BKTreeWords<'a>
         //the top of the stack. 
         let mut nodes_to_process: Vec<_> = Vec::with_capacity(10);
         nodes_to_process.push(self.root_id);
-        let mut best_word: &String = &String::from("");
+        let mut best_word : &[u8] = "".as_bytes();
         let mut best_dist = self.dist_max;
         while nodes_to_process.len() > 0 {
             //By above condition, there is atleast one element in the stack.
             let current_node_id = nodes_to_process.pop().unwrap();
             let current_node = &self.bk_tree[current_node_id]; //maybe use .get syntax here!!
-            let current_dist = (self.dist_fn)(current_node.get().0, word_to_check);
+            let current_dist = (self.dist_fn)(current_node.get().0, word_to_check.as_bytes());
             if current_dist < best_dist {
                 (best_word, best_dist) = (current_node.get().0, current_dist);
             }
@@ -79,7 +82,7 @@ impl<'a> BKTreeWords<'a>
             let children = current_node_id.children(&self.bk_tree);
             
             for child in children {
-                let current_child = &self.bk_tree[child]; //.get syntax??
+                let current_child = &self.bk_tree[child]; 
                 let diff = current_dist - current_child.get().1;
                 let triangle_inequality = diff <= best_dist && diff >= -best_dist;
                 if triangle_inequality {
@@ -89,7 +92,9 @@ impl<'a> BKTreeWords<'a>
             }
 
         }
-        
-        return Some(best_word.clone());
+        match String::from_utf8(best_word.to_vec()) {
+            Ok(w) => return Some(w),
+            Err(_) => return None,
+        }
     }
 }
--- src/med.rs
+++ src/med.rs
@@ -31,26 +31,26 @@ pub fn levenshtien_distance(s1: &str, s2: &str) -> i8 {
     *distances.last().unwrap() as i8
 }
 
-pub fn lev_dist_opt(s1: &str, s2: &str) -> i8 {
+pub fn lev_dist_opt(s1: &[u8], s2: &[u8]) -> i8 {
     //We convert strings into arrays since we need to access
     //the same elements several times
-    let c1: Vec<u8> = s1.bytes().collect();
-    let c2: Vec<u8> = s2.bytes().collect();
+    //let c1: Vec<u8> = s1.bytes().collect();
+    //let c2: Vec<u8> = s2.bytes().collect();
 
     let mut d1: Vec<usize> = vec![0; s2.len()+1]; 
     let mut d2: Vec<usize> = vec![0; s2.len()+1]; 
     
-    for i in 0..c2.len()+1 {
+    for i in 0..s2.len()+1 {
         d1[i] = i;
     }
 
-    for i in 0..c1.len() {
+    for i in 0..s1.len() {
         d2[0] = i + 1;
-        for j in 0..c2.len() {
+        for j in 0..s2.len() {
             let del_cost = d1[j+1] + 1;
             let ins_cost = d2[j] + 1;
             
-            let sub_cost = if c2[j] == c1[i] {
+            let sub_cost = if s2[j] == s1[i] {
                 d1[j]
             } else {
                 d1[j] + 1
@@ -60,7 +60,7 @@ pub fn lev_dist_opt(s1: &str, s2: &str) -> i8 {
         }
         std::mem::swap(&mut d1, &mut d2);
     }
-    return d1[c2.len()] as i8;
+    return d1[s2.len()] as i8;
 }
 
 #[cfg(test)]
